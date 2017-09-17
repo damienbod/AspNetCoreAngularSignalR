0c3c1e3fa1eb993d165326bffe8d10b38149ce26
damienbod <damien_bod@hotmail.com>
2017-09-17 17:26:12 +0200

    removing controller

--- src/AspNetCoreAngularSignalR/SignalRHubs/NewsHub.cs
+++ src/AspNetCoreAngularSignalR/SignalRHubs/NewsHub.cs
@@ -1,8 +1,5 @@
-﻿using AspNetCoreAngularSignalR.Controllers;
-using Microsoft.AspNetCore.SignalR;
-using System;
+﻿using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 
@@ -10,32 +7,15 @@ namespace AspNetCoreAngularSignalR.SignalRHubs
 {
     public class NewssHub : Hub
     {
-        private readonly NewsProvider _newsProvider;
-
-        public NewssHub(NewsProvider newsProvider)
-        {
-            _newsProvider = newsProvider;
-        }
-
         private List<string> groups = new List<string>();
 
         public Task Send(NewsItem newsItem)
         {
-            if(!_newsProvider.GroupExists(newsItem.NewsGroup))
-            {
-                throw new Exception("Group does not exist");
-            }
-
             return Clients.Group(newsItem.NewsGroup).InvokeAsync("Send", newsItem);
         }
 
         public async Task CreateGroup(string groupName)
         {
-            if(!_newsProvider.GroupExists(groupName))
-            {
-                throw new Exception("Group does not exist");
-            }
-
             await Groups.AddAsync(Context.ConnectionId, groupName);
 
             await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} joined {groupName}");
@@ -43,29 +23,13 @@ namespace AspNetCoreAngularSignalR.SignalRHubs
 
         public async Task JoinGroup(string groupName)
         {
-            if(!_newsProvider.GroupExists(groupName))
-            {
-                throw new Exception("Group does not exist");
-            }
-
             await Groups.AddAsync(Context.ConnectionId, groupName);
 
             await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} joined {groupName}");
-
-            var news = _newsProvider.GetAllNewItems(groupName); //.OrderByDescending(o => o.Created);
-            foreach (var item in news)
-            {
-                await Clients.Client(Context.ConnectionId).InvokeAsync("Send", item);
-            }
         }
 
         public async Task LeaveGroup(string groupName)
         {
-            if (!_newsProvider.GroupExists(groupName))
-            {
-                throw new Exception("Group does not exist");
-            }
-
             await Groups.RemoveAsync(Context.ConnectionId, groupName);
 
             await Clients.Group(groupName).InvokeAsync("Send", $"{Context.ConnectionId} left {groupName}");
