f4f5cf2f03765e9d82da39fc926ec5050169eef2
damienbod <damien_bod@hotmail.com>
2017-09-19 04:44:42 +0200

    code clean up, removed unused list

--- src/AspNetCoreAngularSignalR/SignalRHubs/NewsHub.cs
+++ src/AspNetCoreAngularSignalR/SignalRHubs/NewsHub.cs
@@ -6,8 +6,6 @@ namespace AspNetCoreAngularSignalR.SignalRHubs
 {
     public class NewsHub : Hub
     {
-        private List<string> groups = new List<string>();
-
         public Task Send(NewsItem newsItem)
         {
             return Clients.Group(newsItem.NewsGroup).InvokeAsync("Send", newsItem);
@@ -16,7 +14,6 @@ namespace AspNetCoreAngularSignalR.SignalRHubs
         public async Task JoinGroup(string groupName)
         {
             await Groups.AddAsync(Context.ConnectionId, groupName);
-            // $"{Context.ConnectionId} joined {groupName}"
             await Clients.Group(groupName).InvokeAsync("JoinGroup", groupName);
         }
 
