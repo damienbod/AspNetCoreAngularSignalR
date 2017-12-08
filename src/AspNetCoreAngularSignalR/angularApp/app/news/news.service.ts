94ae7b96c3d5eb90ab228137956d194668922692
damienbod <damien_bod@hotmail.com>
2017-12-08 16:21:04 +0100

    adding missing ref

--- src/AspNetCoreAngularSignalR/angularApp/app/news/news.service.ts
+++ src/AspNetCoreAngularSignalR/angularApp/app/news/news.service.ts
@@ -1,3 +1,5 @@
+import 'rxjs/add/operator/map';
+
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
