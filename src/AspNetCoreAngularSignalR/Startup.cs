9e6c2a06cd36a7e2a1518b9d9e886e6d48b8bd96
damienbod <damien_bod@hotmail.com>
2018-05-12 09:15:07 +0200

    code clean up

--- src/AspNetCoreAngularSignalR/Startup.cs
+++ src/AspNetCoreAngularSignalR/Startup.cs
@@ -28,7 +28,6 @@ namespace Angular2WebpackVisualStudio
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<ValidateMimeMultipartContentFilter>();
