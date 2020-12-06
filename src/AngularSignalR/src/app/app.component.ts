50ae8aaeaf0526d9259b074164e0424383ef5907
damienbod <damien_bod@hotmail.com>
2020-12-06 16:11:36 +0100

    updating Angular app

--- src/AngularSignalR/src/app/app.component.ts
+++ src/AngularSignalR/src/app/app.component.ts
@@ -1,9 +1,8 @@
 ﻿import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-    selector: 'app-component',
+    selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 
