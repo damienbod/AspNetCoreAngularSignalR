c4a25e353b0a8050aeb39f6b72df2e2f01490753
damienbod <damien_bod@hotmail.com>
2023-01-04 23:38:16 +0100

    clean up program file

--- src/AspNetCoreAngularSignalR/Program.cs
+++ src/AspNetCoreAngularSignalR/Program.cs
@@ -1,16 +1,70 @@
-namespace AspNetCoreAngularSignalR;
+using AspNetCoreAngularSignalR.Providers;
+using AspNetCoreAngularSignalR;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreAngularSignalR.SignalRHubs;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    serverOptions.AddServerHeader = false;
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+var services = builder.Services;
+var configuration = builder.Configuration;
+var env = builder.Environment;
+
+services.AddTransient<ValidateMimeMultipartContentFilter>();
+
+var sqlConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+services.AddDbContext<NewsContext>(options =>
+    options.UseSqlite(
+        sqlConnectionString
+    ), ServiceLifetime.Singleton
+);
+
+services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder
+                .AllowCredentials()
+                .WithOrigins(
+                    "https://localhost:4200")
+                .SetIsOriginAllowedToAllowWildcardSubdomains()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+services.AddSingleton<NewsStore>();
+
+services.AddSignalR()
+  .AddMessagePackProtocol();
+
+services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+// IdentityModelEventSource.ShowPII = true;
+
+app.UseCors("AllowAllOrigins");
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapHub<LoopyHub>("/loopy");
+app.MapHub<NewsHub>("/looney");
+app.MapHub<LoopyMessageHub>("/loopymessage");
+app.MapHub<ImagesMessageHub>("/zub");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
