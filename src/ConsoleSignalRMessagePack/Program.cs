a437ca51d4e2678ec5953a58594a255f6068fdb6
damienbod <damien_bod@hotmail.com>
2023-01-04 21:46:12 +0100

    Update projects

--- src/ConsoleSignalRMessagePack/Program.cs
+++ src/ConsoleSignalRMessagePack/Program.cs
@@ -1,55 +1,68 @@
 ﻿using Dtos;
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace ConsoleSignalRMessagePack
+namespace ConsoleSignalRMessagePack;
+
+class Program
 {
-    class Program
+    private static HubConnection? _hubConnection;
+
+    public static void Main(string[] args)
     {
-        private static HubConnection _hubConnection;
-
-        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
-
-        static async Task MainAsync()
+        if (args is null)
         {
-            await SetupSignalRHubAsync();
-            _hubConnection.On<MessageDto>("Send", (message) =>
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainAsync()
+    {
+        await SetupSignalRHubAsync();
+        _hubConnection.On<MessageDto>("Send", (message) =>
+        {
+            Console.WriteLine($"Received Message: {message.Name}");
+        });
+        Console.WriteLine("Connected to Hub");
+        Console.WriteLine("Press ESC to stop");
+        do
+        {
+            while (!Console.KeyAvailable)
             {
-                Console.WriteLine($"Received Message: {message.Name}");
-            });
-            Console.WriteLine("Connected to Hub");
-            Console.WriteLine("Press ESC to stop");
-            do
-            {
-                while (!Console.KeyAvailable)
+                var message = Console.ReadLine();
+                if(message != null )
                 {
-                    var message = Console.ReadLine();
-                    await _hubConnection.SendAsync("Send", new MessageDto() { Id = Guid.NewGuid(), Name = message, Amount = 7 });
-                    Console.WriteLine("SendAsync to Hub");
+                    await _hubConnection.SendAsync("Send",
+                    new MessageDto
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = message,
+                        Amount = 7
+                    });
                 }
+
+                Console.WriteLine("SendAsync to Hub");
             }
-            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-
-            await _hubConnection.DisposeAsync();
         }
+        while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
-        public static async Task SetupSignalRHubAsync()
-        {
-            _hubConnection = new HubConnectionBuilder()
-                 .WithUrl("https://localhost:44324/loopymessage")
-                 .AddMessagePackProtocol()
-                 .ConfigureLogging(factory =>
-                 {
-                     factory.AddConsole();
-                     factory.AddFilter("Console", level => level >= LogLevel.Trace);
-                 }).Build();
+        await _hubConnection.DisposeAsync();
+    }
 
-             await _hubConnection.StartAsync();
-        }
+    public static async Task SetupSignalRHubAsync()
+    {
+        _hubConnection = new HubConnectionBuilder()
+             .WithUrl("https://localhost:44324/loopymessage")
+             .AddMessagePackProtocol()
+             .ConfigureLogging(factory =>
+             {
+                 factory.AddConsole();
+                 factory.AddFilter("Console", level => level >= LogLevel.Trace);
+             }).Build();
+
+         await _hubConnection.StartAsync();
     }
 }
